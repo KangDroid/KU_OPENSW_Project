3344e4d772c6b36b75c1d7cda785421af81d825e
KangDroid <hyunwoo286@gmail.com>
2020-05-10 01:20:13 +0900

    New Projecct

---
+++ src/com/kangdroid/scheduling/MainExecuter.java
@@ -0,0 +1,9 @@
+package com.kangdroid.scheduling;
+
+import java.util.Scanner;
+
+public class MainExecuter {
+    public static void main(String[] args) {
+        Scanner sysin = new Scanner(System.in);
+    }
+}
